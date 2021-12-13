a8fdcf0ce90b896c6d09f6a0666429f66a98aecd
TonyPepe <tonybear55665566@gmail.com>
2021-12-13 20:36:09 +0800

    main listview

---
+++ fsbfront/lib/data/shop_item.dart
@@ -0,0 +1,7 @@
+class ShopItem {
+  String name;
+  String detail;
+  String imageUrl;
+
+  ShopItem(this.name, this.detail, this.imageUrl);
+}
--- fsbfront/lib/main.dart
+++ fsbfront/lib/main.dart
@@ -1,43 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:fsbfront/data/shop_item.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<ShopItem> items = [
+    ShopItem("【鬼滅之刃】布製胸章／共8種(隨機單抽)", "",
+        "https://fs1.shop123.com.tw/300324/upload/product/3003243679pic_origin_d27296420209_ars_600_600.jpg"),
+    ShopItem("【hololive】桌墊V2 1st fes~･140宝鐘マリン", "",
+        "https://fs1.shop123.com.tw/300324/upload/product/3003249302pic_origin_5fac63733294_ars_600_600.jpg"),
+    ShopItem("【文豪Stray Dogs】角色吊飾", "",
+        "https://fs1.shop123.com.tw/300324/upload/product/3003249394pic_origin_45786b637058_ars_600_600.jpg"),
+    ShopItem("【Fate/Grand Order】趴娃 vol.4", "",
+        "https://fs1.shop123.com.tw/300324/upload/product/3003249114pic_origin_167d13920630_ars_600_600.jpg"),
+    ShopItem("【鬼滅之刃】2022年台版月曆", "",
+        "https://fs1.shop123.com.tw/300324/upload/product/3003249738pic_origin_373bb0_ars_600_600.jpg"),
+    ShopItem("【女神異聞錄】P25th 含框複製畫", "",
+        "https://fs1.shop123.com.tw/300324/upload/product/3003249711pic_origin_23e4fc257967_ars_600_600.jpg"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Here is so cold"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title"),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.login)),
+          ],
+        ),
+        body: Builder(builder: (context) {
+          return MasonryGridView.count(
+            crossAxisCount: 2,
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Column(
+                  children: [
+                    Image.network(
+                      items[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                    Text(items[index].name)
+                  ],
+                ),
+              );
+            },
+          );
+        }),
       ),
-      body: null,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.shopping_cart),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
