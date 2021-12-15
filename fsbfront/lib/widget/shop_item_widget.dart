e970158e0d7a0e68163093905af5ee1bc1ebc9fc
TonyPepe <tonybear55665566@gmail.com>
2021-12-15 17:12:07 +0800

    feat: Change OpenContainer to AlertDialog

--- fsbfront/lib/widget/shop_item_widget.dart
+++ fsbfront/lib/widget/shop_item_widget.dart
@@ -1,15 +1,38 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:fsbfront/data/shop_item.dart';
 
 class ShopItemWidget extends StatelessWidget {
   const ShopItemWidget(this.item, {Key? key}) : super(key: key);
   final ShopItem item;
+  final String itemImageHeroTag = "ITEM_IMAGE_HERO_TAG";
 
   @override
   Widget build(BuildContext context) {
-    return OpenContainer(
-      closedBuilder: (context, _) => Card(
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: Wrap(
+              children: [
+                Column(
+                  children: [
+                    SizedBox.fromSize(
+                      size: const Size(300, 300),
+                      child: Hero(
+                        tag: itemImageHeroTag,
+                        child: Image.network(item.imageUrl),
+                      ),
+                    ),
+                    Text(item.name),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
+      child: Card(
         clipBehavior: Clip.antiAliasWithSaveLayer,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -18,32 +41,16 @@ class ShopItemWidget extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Image.network(
-                item.imageUrl,
-                fit: BoxFit.cover,
+              Hero(
+                tag: itemImageHeroTag,
+                child: Image.network(
+                  item.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
               Text(item.name)
             ],
           ),
-        ),
-      ),
-      openBuilder: (context, _) => Scaffold(
-        appBar: AppBar(title: Text(item.name)),
-        body: Column(
-          children: [
-            Image.network(item.imageUrl),
-            Align(
-              alignment: Alignment.topRight,
-              child: TextButton(
-                onPressed: () {},
-                child: Wrap(
-                  children: const [
-                    Text("加入購物車"),
-                  ],
-                ),
-              ),
-            )
-          ],
         ),
       ),
     );
