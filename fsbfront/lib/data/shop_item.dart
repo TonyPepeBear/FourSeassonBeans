d6ce3b0ecab9456017ffee26f9386bf6dc67c632
D0843721 <D0843721>
2021-12-19 03:18:50 +0800

    resolve: #9

--- fsbfront/lib/data/shop_item.dart
+++ fsbfront/lib/data/shop_item.dart
@@ -1,7 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fsbfront/data/app_api.dart';
+
 class ShopItem {
   String name;
-  String detail;
+  String description;
   String imageUrl;
+  int price;
+  int count;
 
-  ShopItem(this.name, this.detail, this.imageUrl);
+  ShopItem(this.name, this.description, this.imageUrl, this.price, this.count);
+
+  factory ShopItem.fromJson(Map<String, dynamic> json) {
+    return ShopItem(
+      json['name'] as String,
+      json['description'] as String,
+      json['imageUrl'] as String,
+      json['price'] as int,
+      json['count'] as int,
+    );
+  }
+}
+
+class ShopItemModel extends ChangeNotifier {
+  List<ShopItem> list = List.empty();
+
+  void setList(List<ShopItem> list) {
+    this.list = list;
+    notifyListeners();
+  }
+
+  ShopItemModel() {
+    getAllShopItem().then((value) {
+      setList(value);
+    });
+  }
 }
